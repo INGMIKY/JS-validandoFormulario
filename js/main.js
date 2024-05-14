cb7097cafe34575529b139e3b9feb7bd680755fd
INGMIKY <migueltony9@gmail.com>
2024-05-14 17:40:48 -0600

    validando edad

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 import esUnCuil from "./validarCuil.js";
+import esMayorDeEdad from "./validarEdad.js";
+
 
 const campoDeFormulario = document.querySelectorAll("[required]");
+
 
 
 // El evento blur se refiere al elemento que no tiene focus, es decir, cuando
@@ -12,6 +15,10 @@ campoDeFormulario.forEach((campo)=>{
 function verificarCampo(campo){
     if(campo.name=="cuil" && campo.value.length >=11){
         esUnCuil(campo);
+    }
+
+    if(campo.name == "fecha_nacimiento" && campo.value!= ""){
+        esMayorDeEdad(campo);
     }
 }
 // console.log(campoDeFormulario);
